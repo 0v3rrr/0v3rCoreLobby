d6900457c92b982fb65d5f2182a4c1663d4eda24
The0v3rD0z <60151455+The0v3rD0z@users.noreply.github.com>
2020-08-23 22:56:53 +0200

    Update Main.php

--- src/overa/Main.php
+++ src/overa/Main.php
@@ -35,12 +35,26 @@ class Main extends PluginBase implements Listener{
 
 	
 	public function onJoin(PlayerJoinEvent $event){
-
+		
+	
 		$player = $event->getPlayer();
+		
+		
+		$player->setFood(20);
+		$player->setHealth(20);
+		$player->setGamemode(0);
+		
+		$player->sendMessage("§6============================")
+		$player->sendMessage("§a         Noctalia")
+		$player->sendMessage("§c            En Dev")
+		$player->sendMessage("§6============================")
+		
 		$slot1 = Item::get(360, 0, 1);
 		$slot9 = Item::get(152, 0, 1);
+	
 		$slot1->setCustomName("§aGame Selector");
 		$slot9->setCustomName("§6Profile");
+		
 		$player->getInventory()->clearAll();
 		$player->getInventory()->setItem(0, $slot1);
 		$player->getInventory()->setItem(8, $slot9);
