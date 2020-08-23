29c6828004c3a45b27d00c79a82dc72572dabe19
The0v3rD0z <60151455+The0v3rD0z@users.noreply.github.com>
2020-08-23 23:32:21 +0200

    Update Main.php

--- src/overa/Main.php
+++ src/overa/Main.php
@@ -43,7 +43,7 @@ class Main extends PluginBase implements Listener{
 		$name = $player->getName();
                 $playerrep = $name;
 		
-		$event->setJoinMessage("Test Test")
+		$this->getServer()->broadcastMessage("Tsst");
 		
 		
 		$player->setFood(20);
