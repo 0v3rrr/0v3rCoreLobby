7af7e048fff8a4d0daa40a09d5789b5881736c32
The0v3rD0z <60151455+The0v3rD0z@users.noreply.github.com>
2020-08-24 12:27:33 +0200

    Update Main.php

--- src/overa/Main.php
+++ src/overa/Main.php
@@ -5,89 +5,43 @@ namespace overa;
 use pocketmine\Player;
 use pocketmine\Server;
 
-use pocketmine\command\ConsoleCommandSender;
-
 use pocketmine\plugin\PluginBase;
-
-use pocketmine\inventory\Inventory;
-use pocketmine\inventory\PlayerInventory;
 
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 
 use pocketmine\item\Item;
 
-use muqsit\invmenu\InvMenuHandler;
-use muqsit\invmenu\InvMenu;
-
-use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\event\player\PlayerJoinEvent/
 use pocketmine\event\player\PlayerInteractEvent;
 use pocketmine\event\Listener;
 
-use pocketmine\utils\TextFormat;
-
 class Main extends PluginBase implements Listener{
 
-	
-	public function onEnable(){
 
-		$this->getLogger()->info(" LOBBY CORE ACTIVE");
-
-	}
-
-	
-	public function onJoin (PlayerJoinEvent $event) {
-		
-	
-		$player = $event->getPlayer();
-		$name = $player->getName();
-                $playerrep = $name;
-		
-		$this->getServer()->broadcastMessage("Tsst");
-		
-		
-		$player->setFood(20);
-		$player->setHealth(20);
-		$player->setGamemode(0);
-		
-		$player->sendMessage("§6============================");
-		$player->sendMessage("§a         Noctalia");
-		$player->sendMessage("§c            En Dev");
-		$player->sendMessage("§6============================");
-		
-		$slot1 = Item::get(360, 0, 1);
-		$slot9 = Item::get(152, 0, 1);
-	
-		$slot1->setCustomName("§aGame Selector");
-		$slot9->setCustomName("§6Profile");
-		
-		$player->getInventory()->clearAll();
-		$player->getInventory()->setItem(0, $slot1);
-		$player->getInventory()->setItem(8, $slot9);
-		
-	}
-
-	public function onInteract(PlayerInteractEvent $event){
-
-
-		$player = $event->getPlayer();
-		$item = $event->getItem();
-		$itemname = $player->getInventory()->getItemInHand()->getName();
-	
-		if($item->getId() == 360 || $itemname == "§aGame Selector"){
-			$player->sendMessage("§eNo Game Only for the moments");
-
-			return true;
-		}
-	
-
-		if($item->getId() == 152 || $itemname == "§6Profile"){
-		
-			$player->sendMessage("§eNo Profile On For the moment");
-			return true;
-		
-		}
-		
-
-	}
+public function onEnable(){
+	$this->getLogger()->info(" LOBBY CORE ACTIVE")
 }
+
+public function onJoinPlayer (PlayerJoinEvent $event){
+
+	$player = $event->getPlayer();
+	
+	$name = $event->getPlayer()->getName();
+   $event->setJoinMessage(" ");
+   $player->getInventory()->clearAll();
+
+	$player->getInventory()->setItem(2, Item::get(341)->setCustomName("§r§aLobby"));
+   $player->getInventory()->setItem(4, Item::get(345)->setCustomName("§r§6Compass"));
+   $player->getInventory()->setItem(8, Item::get(130)->setCustomName("§r§5Features"));
+   $player->getInventory()->setItem(6, Item::get(399)->setCustomName("§r§aInfo"));
+   $player->getInventory()->setItem(1, Item::get(160)->setCustomName(" "));
+   $player->getInventory()->setItem(3, Item::get(160)->setCustomName(" "));
+   $player->getInventory()->setItem(5, Item::get(160)->setCustomName(" "));
+   $player->getInventory()->setItem(7, Item::get(160)->setCustomName(" "));
+
+}
+
+
+}
+
