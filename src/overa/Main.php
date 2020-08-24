1a605bada96c1b8fc4d531c4f65aac6b05402147
The0v3rD0z <60151455+The0v3rD0z@users.noreply.github.com>
2020-08-24 12:40:16 +0200

    Update Main.php

--- src/overa/Main.php
+++ src/overa/Main.php
@@ -31,7 +31,8 @@ public function onJoinPlayer(PlayerJoinEvent $event){
    $name = $event->getPlayer()->getName();
    $player->getInventory()->clearAll();
 
-   $player->getInventory()->setItem(2, Item::get(341)->setCustomName("§r§aLobby"));
+   $player->getInventory()->setItem(0, Item::get(449)->setCustomName("§r§aProfile");
+   $player->getInventory()->setItem(2, Item::get(341)->setCustomName("§r§eLobby"));
    $player->getInventory()->setItem(4, Item::get(345)->setCustomName("§r§6Compass"));
    $player->getInventory()->setItem(8, Item::get(130)->setCustomName("§r§5Features"));
    $player->getInventory()->setItem(6, Item::get(399)->setCustomName("§r§aInfo"));
